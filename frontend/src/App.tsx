0340cb9640c061a6b56b5c71f98b04e032362c12
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-20 01:47:09 -0300

    building routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
