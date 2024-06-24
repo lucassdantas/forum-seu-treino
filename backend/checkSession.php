be75339c0005d7e8e288bdeed8fea7ed4c8af02f
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-24 18:38:07 -0300

    login working

--- backend/checkSession.php
+++ backend/checkSession.php
@@ -3,6 +3,7 @@ session_start();
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: http://localhost:5173'); // Substitua pelo URL do seu frontend
 header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Content-Type');
 
 $response = array('loggedIn' => false);
 
