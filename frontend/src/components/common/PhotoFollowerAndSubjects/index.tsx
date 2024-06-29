9fae36f95f7fb222871e03c0d07852421fdb97bc
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-29 00:36:10 -0300

    build profile info on feed

--- frontend/src/components/common/PhotoFollowerAndSubjects/index.tsx
+++ frontend/src/components/common/PhotoFollowerAndSubjects/index.tsx
@@ -12,11 +12,11 @@ export const PhotoFollowerAndSubjects = ({profilePhoto, profileName,  followers,
     <div className='bg-black text-white flex justify-center -mt-8'>
       <Limiter>
       <div className='flex items-center py-4 w-full '>
-        <div className="flex gap-4 w-2/3 items-center">
-          <img src={profilePhoto} alt='Foto de perfil' className='w-[150px] '/>
-          <span className='font-bold text-lg'>{profileName}</span>
+        <div className="flex gap-6 w-2/3 items-center">
+          <img src={profilePhoto} alt='Foto de perfil' className='w-[180px] '/>
+          <span className='font-bold text-xl'>{profileName}</span>
         </div>
-        <div className="flex w-1/3 gap-8">
+        <div className="flex w-1/3 gap-8 justify-center">
           <div className="flex flex-col items-center">
             <span>Seguidores</span>
             <span className='font-bold'>{followers}</span>
