601d151c2660e8c71b303ec475431eaee01e3f90
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-04 22:52:40 -0300

    feed > add new post by front end working

--- frontend/src/components/common/Button/index.tsx
+++ frontend/src/components/common/Button/index.tsx
@@ -3,19 +3,20 @@ import React, { ReactNode } from 'react'
 type ButtonProps = {
     children:ReactNode;
     className?:string;
+    onClick?:() => void;
 }
 
-export const Button = ({children, className}:ButtonProps) => {
+export const Button = ({children, className, onClick}:ButtonProps) => {
   return (
-    <div className={`bg-orange-seu-treino rounded-lg text-center text-black px-2 py-2 max-w-[160px] hover:bg-yellow-seu-treino transition-colors font-semibold cursor-pointer text-lg ${className}`}>
+    <div onClick={() => onClick?.()} className={`bg-orange-seu-treino rounded-lg text-center text-black px-2 py-2 max-w-[160px] hover:bg-yellow-seu-treino transition-colors font-semibold cursor-pointer text-lg ${className}`}>
        <span>{children}</span>
     </div>
   )
 }
 
-export const OutlineButton = ({children, className}:ButtonProps) => {
+export const OutlineButton = ({children, className, onClick}:ButtonProps) => {
   return (
-    <div className={`bg-transparent border-2 rounded-lg text-center px-2 py-2 max-w-[160px] hover:bg-yellow-seu-treino transition-colors font-semibold cursor-pointer ${className}`}>
+    <div onClick={() => onClick?.()} className={`bg-transparent border-2 rounded-lg text-center px-2 py-2 max-w-[160px] hover:bg-yellow-seu-treino transition-colors font-semibold cursor-pointer ${className}`}>
        <span>{children}</span>
     </div>
   )
---
+++ frontend/src/pages/Feed/components/PostCard/index.tsx
@@ -0,0 +1,45 @@
+import { GrayCard } from '@/components/common/Card'
+import { formatTimeAgo } from '@/utils/formatTimeAgo'
+import React from 'react'
+import { CiHeart } from 'react-icons/ci'
+import { TfiComment } from 'react-icons/tfi'
+
+type PostCardProps = {
+    post:any
+}
+
+export const PostCard = ({post}:PostCardProps) => {
+  return (
+    <GrayCard>
+    <div className="flex gap-4">
+
+      <div className="flex flex-col ">
+          <img src={post.authorImage} alt='Foto do autor do post' className='w-[50px]'/>
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <span>{post.author}</span>
+        <span className='text-sm opacity-85'>{formatTimeAgo(post.dateOfCreation)}</span>
+      </div>
+
+    </div>
+
+    <div className='my-4'>
+      <p>{post.postContent}</p>
+    </div>
+
+    <div className='flex border-t border-t-neutral-700 py-4 gap-4'>
+      <div className='flex items-center gap-2'>
+        <CiHeart className='text-xl cursor-pointer'/> <span>Curtidas {post.likesQuantity}</span>
+      </div>
+      <div className='flex items-center gap-2'>
+        <TfiComment className='cursor-pointer'/> <span>Comentários {post.commentsQuantity}</span>
+      </div>
+    </div>
+
+    <div className='border-t'>
+
+    </div>
+  </GrayCard>
+  )
+}
