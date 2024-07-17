ac00b6df9cac375e30a5a42e18f8ed4b1908a628
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-16 23:48:19 -0300

    finish followerCard

--- frontend/src/components/UserInFollowContextCard/index.tsx
+++ frontend/src/components/UserInFollowContextCard/index.tsx
@@ -4,17 +4,16 @@ import { Link } from 'react-router-dom'
 
 export const UserInFollowContextCard = ({user, className}:{user:UserInFollowContextProps, className?:string}) => {
   return (
-    <div className={'bg-neutral-950 text-white rounded-md' + className}>
-      <div className={`bg-[url("${user.coverPhoto} bg-neutral-400 w-full h-12 p-1")]`}> </div>
-      <div className='p-4 flex flex-col items-center gap-4 w-full'>
-        <img src={user.userImage } alt={'Foto de perfil do' + user.userName} className='w-[120px] -mt-12 text-center relative'/>
-        <span>{user.userName}</span>
+    <div className={'bg-neutral-950 text-white rounded-md ' + className}>
+      <div className={`bg-[url("${user.coverPhoto} bg-neutral-400 w-full h-[80px] p-1")]`}> </div>
+      <div className='py-8 flex flex-col items-center gap-4 w-full'>
+        <img src={user.userImage } alt={'Foto de perfil do' + user.userName} className='w-[120px] -mt-14 text-center relative'/>
+        <span className='mb-4'>{user.userName}</span>
         <div className='flex justify-center gap-4'>
           <Button className='bg-yellow-seu-treino'>Mensagem</Button>
           <OutlineButton className='border-yellow-seu-treino min-w-[90px]'>Seguir</OutlineButton>
         </div>
         <Link to={`user.userId`}>Visitar Perfil</Link>
-
       </div>
     </div>
   )
