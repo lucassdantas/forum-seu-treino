3ca3fb053bd0ebff689637aaf763870d3ce078cc
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-17 20:14:42 -0300

    trying to fetch posts

--- backend/index.php
+++ backend/index.php
@@ -1,25 +1,35 @@
 <?php
-$request = $_SERVER['REQUEST_URI'];
-$request = explode('/', trim($request, '/'));
+include_once './config/cors.php';
+include_once './config/db.php';
 
-$resource = $request[0];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$requestUri = $_SERVER['REQUEST_URI'];
 
-switch ($resource) {
-    case 'users':
-        require __DIR__ . '/controllers/userController.php';
-        break;
-    case 'topics':
-        require __DIR__ . '/controllers/topicController.php';
-        break;
-    case 'posts':
-        require __DIR__ . '/controllers/postController.php';
-        break;
-    case 'comments':
-        require __DIR__ . '/controllers/commentController.php';
-        break;
-    default:
-        http_response_code(404);
-        echo json_encode(["message" => "Resource not found"]);
-        break;
+$uriSegments = explode('/', parse_url($requestUri, PHP_URL_PATH));
+if (isset($uriSegments[1])) {
+    switch ($uriSegments[1]) {
+        case 'users':
+            include_once 'controllers/userController.php';
+            break;
+        case 'topics':
+            include_once 'controllers/topicController.php';
+            break;
+        case 'posts':
+            include_once 'controllers/postController.php';
+            break;
+        case 'comments':
+            include_once 'controllers/commentController.php';
+            break;
+        case 'likes':
+            include_once 'controllers/likeController.php';
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode(['message' => 'Endpoint not found']);
+            break;
+    }
+} else {
+    http_response_code(404);
+    echo json_encode(['message' => 'Endpoint not found']);
 }
 ?>
