8dd41c9e8413a040aed7b164f5c51969a2737d06
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-17 22:44:20 -0300

    fetch posts working, but need fixes

---
+++ backend/postController.php
@@ -0,0 +1,78 @@
+<?php
+include_once 'config/cors.php';
+include_once 'config/db.php';
+include_once 'models/post.php';
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+$database = new Database();
+$db = $database->getConnection();
+
+$post = new Post($db);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch($method) {
+    case 'POST':
+        $data = json_decode(file_get_contents("php://input"));
+
+        $post->postTopicId = $data->postTopicId;
+        $post->authorId = $data->authorId;
+        $post->postContent = $data->postContent;
+        $post->dateOfCreation = date('Y-m-d H:i:s'); // Supondo que a data de criação é a data atual
+        $post->postImage = $data->postImage;
+        $post->postLikesQuantity = $data->postLikesQuantity;
+        $post->postCommentsQuantity = $data->postCommentsQuantity;
+
+        if ($post->create()) {
+            echo json_encode(["message" => "Post was created."]);
+        } else {
+            echo json_encode(["message" => "Unable to create post."]);
+        }
+        break;
+
+    case 'GET':
+        $stmt = $post->read();
+        $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($posts);
+        break;
+
+    case 'PUT':
+        $data = json_decode(file_get_contents("php://input"));
+
+        $post->postId = $data->postId;
+        $post->postTopicId = $data->postTopicId;
+        $post->authorId = $data->authorId;
+        $post->postContent = $data->postContent;
+        $post->dateOfCreation = $data->dateOfCreation;
+        $post->postImage = $data->postImage;
+        $post->postLikesQuantity = $data->postLikesQuantity;
+        $post->postCommentsQuantity = $data->postCommentsQuantity;
+
+        if ($post->update()) {
+            echo json_encode(["message" => "Post was updated."]);
+        } else {
+            echo json_encode(["message" => "Unable to update post."]);
+        }
+        break;
+
+    case 'DELETE':
+        $data = json_decode(file_get_contents("php://input"));
+
+        $post->postId = $data->postId;
+
+        if ($post->delete()) {
+            echo json_encode(["message" => "Post was deleted."]);
+        } else {
+            echo json_encode(["message" => "Unable to delete post."]);
+        }
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(["message" => "Method not allowed"]);
+        break;
+}
+?>
