3a383c238d97d452991c4aa8add17be85e9d2e7e
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-18 10:49:07 -0300

    add get user by id to model, controller and api/users. need to test

--- backend/checkSession.php
+++ backend/checkSession.php
@@ -1,22 +1,19 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-include_once './config/cors.php'; // Inclui configuração de CORS
+include_once './config/cors.php'; 
 
-// Verifica se o usuário está logado
 if (!isset($_SESSION['userId'])) {
     http_response_code(401);
     echo json_encode(['error' => 'Usuário não autenticado']);
     exit;
 }
 if (isset($_SESSION['userId'])) {
-    // Definir os dados do usuário novamente na sessão
     $response['loggedIn'] = true;
     $_SESSION['userId']     = $user['userId'];
     $_SESSION['userEmail']  = $user['userEmail'];
     $_SESSION['userName']   = $user['userName'];
     $response['success']    = true;
-    // Definir outros dados conforme necessário
 }
 
 echo json_encode($response);
