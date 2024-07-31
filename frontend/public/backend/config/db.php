4e83172c4f01087205e9176538432e0d94f8d4e6
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-31 20:38:10 -0300

    edit db

--- frontend/public/backend/config/db.php
+++ frontend/public/backend/config/db.php
@@ -1,21 +1,3 @@
 <?php
-class Database {
-    private $host = 'localhost';
-    private $db_name = 'forum_seutreino';
-    private $db_username = 'root';
-    private $db_password = '';
-    public $conn;
-
-    public function getConnection() {
-        $this->conn = null;
-
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->db_username, $this->db_password);
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-
-        return $this->conn;
-    }
-}
+require_once './devDb.php';
+#require_once './productionDb.php';
