a64a0964c67e94ffdfd1bc3b52b41c0eb6a0f8aa
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-31 18:09:29 -0300

    cahnge follower model

--- frontend/public/backend/models/follower.php
+++ frontend/public/backend/models/follower.php
@@ -31,7 +31,11 @@ class Follower {
 
     // Read
     public function read() {
-        $query = "SELECT * FROM " . $this->table_name;
+        $query = "SELECT f.followerUserFollower, f.followerUserFollowed
+        FROM " . $this->table_name . " f
+        JOIN forum_users u1 ON f.followerUserFollower = u1.userId
+        JOIN forum_users u2 ON f.followerUserFollowed = u2.userId
+        WHERE u1.userStatus = 1 OR u2.userStatus = 1;";
 
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -41,7 +45,7 @@ class Follower {
 
     // Get followers by followerId
     public function getFollowersByFollowerId($followerUserFollower) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE followerUserFollower = :followerUserFollower";
+        $query = "SELECT *, userStatus FROM " . $this->table_name . " INNER JOIN forum_users ON userId = followerUserFollower WHERE followerUserFollower = :followerUserFollower AND userStatus = 1";
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":followerUserFollower", $followerUserFollower);
@@ -52,7 +56,7 @@ class Follower {
 
     // Get followed by followedId
     public function getFollowedByFollowedId($followerUserFollowed) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE followerUserFollowed = :followerUserFollowed";
+        $query = "SELECT *, userStatus FROM " . $this->table_name . " INNER JOIN forum_users ON userStatus = followerUserFollowed WHERE followerUserFollowed = :followerUserFollowed AND userStatus = 1";
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":followerUserFollowed", $followerUserFollowed);
@@ -63,10 +67,10 @@ class Follower {
 
     // Get followers list
     public function getFollowersList($followerUserFollowed) {
-        $query = "SELECT u.userId, u.userName 
+        $query = "SELECT u.userId, u.userName, u.userStatus 
                   FROM " . $this->table_name . " f
                   JOIN forum_users u ON f.followerUserFollower = u.userId
-                  WHERE f.followerUserFollowed = :followerUserFollowed";
+                  WHERE f.followerUserFollowed = :followerUserFollowed AND u.userStatus = 1";
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":followerUserFollowed", $followerUserFollowed);
@@ -77,10 +81,10 @@ class Follower {
 
     // Get following list
     public function getFollowingList($followerUserFollower) {
-        $query = "SELECT u.userId, u.userName 
+        $query = "SELECT u.userId, u.userName, u.userStatus 
                   FROM " . $this->table_name . " f
                   JOIN forum_users u ON f.followerUserFollowed = u.userId
-                  WHERE f.followerUserFollower = :followerUserFollower";
+                  WHERE f.followerUserFollower = :followerUserFollower AND u.userStatus = 1";
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":followerUserFollower", $followerUserFollower);
@@ -91,7 +95,7 @@ class Follower {
 
     // Check if user is a follower
     public function isFollower($followerUserFollower, $followerUserFollowed) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE followerUserFollower = :followerUserFollower AND followerUserFollowed = :followerUserFollowed";
+        $query = "SELECT *, userStatus FROM " . $this->table_name . " INNER JOIN forum_users ON userId = followerUserFollower WHERE followerUserFollower = :followerUserFollower AND followerUserFollowed = :followerUserFollowed AND userStatus = 1";
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":followerUserFollower", $followerUserFollower);
