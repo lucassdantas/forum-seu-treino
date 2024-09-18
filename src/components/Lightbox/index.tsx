fe37cab9a5e153df17a6461826fa8feec5ae2038
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-17 22:22:53 -0300

    fix post image on vertical iamges

--- src/components/Lightbox/index.tsx
+++ src/components/Lightbox/index.tsx
@@ -12,10 +12,10 @@ export const Lightbox: React.FC<LightboxProps> = ({ imageUrl }) => {
 
   return (
     <>
-      <img src={imageUrl} onClick={openLightbox} className="cursor-pointer max-h-[250px] object-cover w-full rounded" />
+      <img src={imageUrl} onClick={openLightbox} className="cursor-pointer max-h-[550px] object-cover w-full rounded" />
       {isOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-start justify-center z-50 w-full" onClick={closeLightbox}>
-          <div className="flex justify-center items-center w-[80%] h-[80%] overflow-scroll ">
+          <div className="flex justify-center items-center w-[80%] h-fit overflow-scroll ">
             <button onClick={closeLightbox} className="absolute top-0 lg:top-5 right-5 text-white text-3xl">&times;</button>
             <img src={imageUrl} className="max-w-full max-h-full h-full w-full object-contain" />
           </div>
