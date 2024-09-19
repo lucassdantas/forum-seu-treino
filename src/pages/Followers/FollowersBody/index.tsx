ee868a369cfe87844992ef424bf5e901ca3422c1
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-09-19 09:02:07 -0300

    fix photo error on update user

--- src/pages/Followers/FollowersBody/index.tsx
+++ src/pages/Followers/FollowersBody/index.tsx
@@ -122,7 +122,7 @@ export const FollowersBody = () => {
         userPhone,
         userBirthday,
         userPassword,
-        userHasImage: !!profileImage,
+        userHasImage: !!previewImage,
         userRole,
       };
   
